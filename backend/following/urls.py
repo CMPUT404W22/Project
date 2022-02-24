ee9513566640488217a41f84d837374d2b97a752
celina <csheng2@ualberta.ca>
2022-02-24 11:46:02 -0700

    skeleton code for following view and template urls

--- backend/following/urls.py
+++ backend/following/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 
-urlpatterns = [
+url_base = 'service/authors'
 
+urlpatterns = [
+    path('{url_base}/{AUTHOR_ID}/followers/', views.index, name='main-view'),
+    path('{url_base}/{AUTHOR_ID}/following/', views.index, name='main-view'),
+    path('{url_base}/{AUTHOR_ID}/addFollower/', views.index, name='main-view'),
+    path('{url_base}/{AUTHOR_ID}/removeFollower/', views.index, name='main-view'),
+    path('{url_base}/{AUTHOR_ID}/addFollowing/', views.index, name='main-view'),
+    path('{url_base}/{AUTHOR_ID}/removeFollowing/', views.index, name='main-view'),
 ]
--- backend/following/views.py
+++ backend/following/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+def getFollowers():
+    # 'service/authors/{AUTHOR_ID}/followers/'
+    pass
+
+def getFollowing():
+    # 'service/authors/{AUTHOR_ID}/following/'
+    pass
+
+def addFollower():
+    # 'service/authors/{AUTHOR_ID}/addFollower/'
+    pass
+
+def removeFollower():
+    # 'service/authors/{AUTHOR_ID}/removeFollower/'
+    pass
+
+def addFollowing():
+    # 'service/authors/{AUTHOR_ID}/addFollowing/'
+    pass
+
+def removeFollowing():
+    # 'service/authors/{AUTHOR_ID}/removeFollowing/'
+    pass
