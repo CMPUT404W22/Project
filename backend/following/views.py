e73666792d82276c0c769281e5bf5f500807428a
celina <csheng2@ualberta.ca>
2022-02-24 15:55:29 -0700

    sample code

--- backend/following/views.py
+++ backend/following/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
+from .models import Following
+from django.views import generic
+
+class IndexView(generic.ListView):
+    #template_name = 'polls/index.html'
+    #context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Following.objects.order_by('following')
+
+class DetailView(generic.DetailView):
+    model = Following
+    template_name = 'polls/detail.html'
 
 # Create your views here.
 def getFollowers():
     # 'service/authors/{AUTHOR_ID}/followers/'
     pass
 
-def getFollowing():
+def getFollowing(author_id):
+    following = Following.objects.get(author=author_id)
+    return following.following
     # 'service/authors/{AUTHOR_ID}/following/'
-    pass
 
 def addFollower():
     # 'service/authors/{AUTHOR_ID}/addFollower/'
