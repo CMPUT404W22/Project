5e5b73750c405f9dd310ddbe8df83754bacc62f9
Anita F <anitaf@Anitas-MacBook-Pro.local>
2022-03-03 16:59:38 -0700

    fixed problems in followers api and moved to following/view

--- backend/author/urls.py
+++ backend/author/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from author import views
+from following import views as followingViews
 
 urlpatterns = [
     # path('register/', views.Register.as_view(), name="register"),
     path('', views.GetAuthorsApiView.as_view(), name="authors"),
     path('<str:user_id>/', views.GetAuthorApiView.as_view(), name="author"),
-    path('<str:user_id>/followers', views.GetAuthorFollowersApiView.as_view(), name="author follower"),
-    path('<str:user_id>/followers/<str:foreign_user_id>', views.GetAuthorFollowersApiView.as_view(), name = "delete follower")
+    path('<str:user_id>/followers', followingViews.GetFollowersApiView.as_view(), name="get follower"),
+    path('<str:user_id>/followers/<str:foreign_user_id>', followingViews.EditFollowersApiView.as_view(), name = "edit follower")
 ]
--- backend/following/views.py
+++ backend/following/views.py
@@ -6,24 +6,66 @@ from rest_framework import response, status
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.generics import GenericAPIView
 
+from author.models import Author
 from following.models import Following
 
 
-class FollowersApiView(GenericAPIView):
+class GetFollowersApiView(GenericAPIView):
     authentication_classes = [BasicAuthentication, ]
 
-    def get(self, request, *args, **kwargs):
-        pass
+    def get(self, request, user_id):
+        # gets a list of authors who are user_id's followers
+        try:
+            author = Author.objects.get(id=user_id)
+            followers = Following.objects.filter(following=author)
+            result = {
+                "type": "followers",
+                "items": [follower.author.toJson() for follower in followers]
+            }
+            return response.Response(result, status.HTTP_200_OK)
 
-    def delete(self, request, *args, **kwargs):
-        # remove FOREIGN_AUTHOR_ID as a follower of AUTHOR_ID
-        pass
+        except Exception as e:
+            return response.Response(status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, *args, **kwargs):
-        pass
+class EditFollowersApiView(GenericAPIView):
+    authentication_classes = [BasicAuthentication, ]
 
-    def get(self, request, *args, **kwargs):
-        pass
+    def delete(self, request, user_id, foreign_user_id):
+        # remove FOREIGN_AUTHOR_ID as a follower of AUTHORs_ID
+        try:
+            follower = Author.objects.get(id=foreign_user_id)
+            author = Author.objects.get(id=user_id)
+            record = Following.objects.get(author=follower, following=author)
+            record.delete()
+            return response.Response("Deleted", status.HTTP_200_OK)
+        except Exception as e:
+            return response.Response("Error while trying to delete", status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, user_id, foreign_user_id):
+        # Add FOREIGN_AUTHOR_ID as follower of AUTHOR_ID
+        try:
+            author = Author.objects.get(id=user_id)
+            follower = Author.objects.get(id=foreign_user_id)
+            Following.objects.create(author=follower, following=author)
+            return response.Response("Added", status.HTTP_200_OK)
+        except Exception as e:
+            return response.Response("Error while trying to add", status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, user_id, foreign_user_id):
+        # check if FOREIGN_AUTHOR_ID is a follower of AUTHOR_ID
+        try:
+            author = Author.objects.get(id=user_id)
+            unfollower = Author.objects.get(id=foreign_user_id)
+            followers = Following.objects.filter(following=author)
+            if followers:
+                for follower in followers:
+                    if follower.author == unfollower:
+                        return response.Response("True", status.HTTP_200_OK)
+
+            return response.Response("False", status.HTTP_200_OK)
+        except Exception as e:
+            return response.Response("Error while trying to get followers", status=status.HTTP_400_BAD_REQUEST)
+
 
 class FollowingApiView(GenericAPIView):
     authentication_classes = [BasicAuthentication, ]
@@ -31,21 +73,26 @@ class FollowingApiView(GenericAPIView):
     def get(self, request):
         pass
 
+
 def test(request, author_username):
     followers = Following.objects.all()
     return HttpResponse(str(followers))
+
 
 def addFollower(request):
     # 'service/authors/{AUTHOR_ID}/addFollower/'
     pass
 
+
 def removeFollower(request):
     # 'service/authors/{AUTHOR_ID}/removeFollower/'
     pass
 
+
 def addFollowing():
     # 'service/authors/{AUTHOR_ID}/addFollowing/'
     pass
+
 
 def removeFollowing():
     # 'service/authors/{AUTHOR_ID}/removeFollowing/'
