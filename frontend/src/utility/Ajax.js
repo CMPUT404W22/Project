3e8543256b7a11d143d62b2c70fd42282c99d0e9
yuqian5 <28016308+yuqian5@users.noreply.github.com>
2022-03-04 13:06:02 -0700

    login added

--- frontend/src/utility/Ajax.js
+++ frontend/src/utility/Ajax.js
@@ -1,98 +1,76 @@
 import $ from "jquery";
 import {AuthProvider} from "../auth/AuthProvider";
+import axios from "axios";
 
 const BackendAddress = "http://127.0.0.1:8000/";
 
 class Ajax {
     /**
-     * Ajax post request
-     * @param endpoint {string}
-     * @param data {string}
-     * @param success {function}
-     * @param failure {function}
-     * @param always {function}
+     * Get
+     * @param path {string}
+     * @returns {Promise<AxiosResponse<any>>}
      */
-    static post (endpoint, data, success = ()=>{}, failure = ()=>{}, always = ()=>{}) {
-        AuthProvider.refreshToken().then(() => {
-            $.ajax({
-                url: BackendAddress + endpoint,
-                type: 'POST',
-                contentType: "application/json",
-                dataType: "json",
-                data: data,
-                headers: {"Authorization": `Bearer ${localStorage.getItem('token')}`},
-                success: success,
-                error: failure,
-                always: always
-            });
+    static async get(path){
+        return axios.get(BackendAddress + path, {
+            auth: {
+                username: localStorage.getItem("username"),
+                password: localStorage.getItem("password")
+            }
         });
     }
 
     /**
-     * Ajax get request
-     * @param endpoint {string}
-     * @param success {function}
-     * @param failure {function}
-     * @param always {function} 
-     * */
-    static get(endpoint, success = () => { }, failure = () => { }, always = () => { }) {
-        AuthProvider.refreshToken().then(() => {
-            $.ajax({
-                url: BackendAddress + endpoint,
-                type: 'GET',
-                headers: { "Authorization": `Bearer ${localStorage.getItem('token')}` },
-                success: success,
-                error: failure,
-                always: always
-            });
-        });
-    }
-
-    /**
-     * Ajax post request with no token refresj
-     * @param endpoint {string}
-     * @param data {string}
-     * @param success {function}
-     * @param failure {function}
-     * @param always {function}
-     * @returns {Promise<*>}
+     * Post
+     * @param path {string}
+     * @param data {object}
+     * @returns {Promise<AxiosResponse<any>>}
      */
-    static postNoRefresh (endpoint, data, success = ()=>{}, failure = ()=>{}, always = ()=>{}) {
-        return $.ajax({
-            url: BackendAddress + endpoint,
-            type: 'POST',
-            contentType: "application/json",
-            dataType: "json",
-            data: data,
-            headers: {"Authorization": `Bearer ${localStorage.getItem('token')}`},
-            success: success,
-            error: failure,
-            always: always
+    static async post(path, data){
+        return axios.post(BackendAddress + path, data, {
+            auth: {
+                username: localStorage.getItem("username"),
+                password: localStorage.getItem("password")
+            }
         });
     }
 
     /**
-  * Ajax put request
-  * @param endpoint {string}
-  * @param data {string}
-  * @param success {function}
-  * @param failure {function}
-  * @param always {function}
-  */
-    static put (endpoint, data, success = () => { }, failure = () => { }, always = () => { }) {
-        AuthProvider.refreshToken().then(() => {
-            $.ajax({
-                url: BackendAddress + endpoint,
-                type: 'PUT',
-                contentType: "application/json",
-                dataType: "json",
-                data: data,
-                headers: { "Authorization": `Bearer ${localStorage.getItem('token')}` },
-                success: success,
-                error: failure,
-                always: always
-            });
-        })
+     * Post No Auth
+     * @param path {string}
+     * @param data {object}
+     * @returns {Promise<AxiosResponse<any>>}
+     */
+    static async postNoAuth(path, data){
+        return axios.post(BackendAddress + path, data);
+    }
+
+    /**
+     * Put
+     * @param path {string}
+     * @param data {object}
+     * @returns {Promise<AxiosResponse<any>>}
+     */
+    static async put(path, data){
+        return axios.put(BackendAddress + path, data, {
+            auth: {
+                username: localStorage.getItem("username"),
+                password: localStorage.getItem("password")
+            }
+        });
+    }
+
+    /**
+     * Delete
+     * @param path {string}
+     * @returns {Promise<AxiosResponse<any>>}
+     */
+    static async delete(path){
+        return axios.put(BackendAddress + path, {
+            auth: {
+                username: localStorage.getItem("username"),
+                password: localStorage.getItem("password")
+            }
+        });
     }
 }
 
