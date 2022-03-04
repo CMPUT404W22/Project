43a9e3d02677b30dda563e722ef1afea65324f33
celina <csheng2@ualberta.ca>
2022-03-04 08:53:42 -0700

    delete all notification records from the author

--- backend/notification/views.py
+++ backend/notification/views.py
@@ -23,3 +23,14 @@ class NotificationsApiView(GenericAPIView):
 
         except Exception as e:
             return response.Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, user_id):
+        # remove FOREIGN_AUTHOR_ID as a follower of AUTHORs_ID
+        try:
+            author = Author.objects.get(id=user_id)
+            notifications = Notification.objects.filter(author=author)
+            for n in notifications:
+                notifications.delete()
+            return response.Response("Deleted", status.HTTP_200_OK)
+        except Exception as e:
+            return response.Response(f"Error while trying to delete: {e}", status=status.HTTP_404_NOT_FOUND)
