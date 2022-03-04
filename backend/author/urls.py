cb12705518f76a1404369ac28ab0a3cc816f0ec4
celina <csheng2@ualberta.ca>
2022-03-04 08:51:49 -0700

    basic get

--- backend/author/urls.py
+++ backend/author/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from author import views
 from following import views as followingViews
+from notification import views as notificationViews
 
 urlpatterns = [
     # path('register/', views.Register.as_view(), name="register"),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('<str:user_id>/', views.GetAuthorApiView.as_view(), name="author"),
     path('<str:user_id>/followers', followingViews.GetFollowersApiView.as_view(), name="get follower"),
     path('<str:user_id>/followers/<str:foreign_user_id>', followingViews.EditFollowersApiView.as_view(), name = "edit follower")
+    path('<str:user_id>/inbox', notificationViews..as_view(), name = "edit follower")
 ]
--- backend/notification/views.py
+++ backend/notification/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.generics import GenericAPIView
+from rest_framework import response, status
+from author.models import Author
+from notification.models import Notification
+
 
 # Create your views here.
+class NotificationsApiView(GenericAPIView):
+    authentication_classes = [BasicAuthentication, ]
+
+    def get(self, request, user_id):
+        # gets a list of authors who are user_id's followers
+        try:
+            author = Author.objects.get(id=user_id)
+            notifications = Notification.objects.filter(author=author)
+            result = {
+                "type": "inbox",
+                "items": [n.content.toJson() for n in notifications]
+            }
+            return response.Response(result, status.HTTP_200_OK)
+
+        except Exception as e:
+            return response.Response(status=status.HTTP_400_BAD_REQUEST)
