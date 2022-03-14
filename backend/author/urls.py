f753937782ef637174208b179792c2d9a4011cc7
celina <csheng2@ualberta.ca>
2022-03-14 17:53:12 -0600

    remove url patterns from notifications and move to author

--- backend/author/urls.py
+++ backend/author/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('<str:user_id>/liked', likesViews.GetLikedApiView.as_view(), name="liked"),
     path('<str:user_id>/followers', followingViews.GetFollowersApiView.as_view(), name="get follower"),
     path('<str:user_id>/followers/<str:foreign_user_id>', followingViews.EditFollowersApiView.as_view(), name="edit follower"),
-    #path('<str:user_id>/inbox', notificationViews..as_view(), name = "edit follower")
+    path('<str:user_id>/inbox', notificationViews.NotificationsApiView.as_view(), name = "get inbox")
 ]
--- backend/notification/urls.py
+++ backend/notification/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 from notification import views as notificationViews
 
 urlpatterns = [
-    # NOTE: "notifcation_id" is NOT the id of the notification object. It is the id of the post/follow/like/comment
-    path('inbox/<str:user_id>/<str:notification_type>/<str:notifcation_id>', notificationViews.NotificationsApiView.as_view(), name="post notifaction to inbox")
 ]
