229084962138a2b774d4d323b0e9674e887a1358
celina <csheng2@ualberta.ca>
2022-03-14 17:34:45 -0600

    fix Notifcation model (change field type for notifcation_type and notification_id)

--- backend/notification/models.py
+++ backend/notification/models.py
@@ -8,6 +8,6 @@ from author.models import Author
 class Notification(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     author = models.ForeignKey(Author, blank=False, null=False, on_delete=models.CASCADE)
-    notification_type = models.TextField(blank=False, null=False)
-    notification_id = models.TextField(blank=False, null=False)
+    notification_type = models.CharField(max_length=20, blank=False, null=False, default=False)
+    notification_id = models.UUIDField(blank=False, null=False, default=False)
     created = models.DateTimeField(auto_now_add=True)
