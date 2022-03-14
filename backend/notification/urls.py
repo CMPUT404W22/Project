f850271eccf69b2c4a9479821d9327ef0f27d771
celina <csheng2@ualberta.ca>
2022-03-14 16:49:36 -0600

    add url to make notification post request

--- backend/notification/urls.py
+++ backend/notification/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from notification import views as notificationViews
 
 urlpatterns = [
-
+    # NOTE: "notifcation_id" is NOT the id of the notification object. It is the id of the post/follow/like/comment
+    path('inbox/<str:user_id>/<str:notification_type>/<str:notifcation_id>', notificationViews.NotificationsApiView.as_view(), name="post notifaction to inbox")
 ]
