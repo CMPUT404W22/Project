724fb34e8d63f93fb15da9fb9c85944d6b858d36
celina <csheng2@ualberta.ca>
2022-03-14 16:49:13 -0600

    modify notification database

--- backend/notification/models.py
+++ backend/notification/models.py
@@ -8,5 +8,6 @@ from author.models import Author
 class Notification(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     author = models.ForeignKey(Author, blank=False, null=False, on_delete=models.CASCADE)
-    content = models.TextField(blank=True, null=True)
+    notification_type = models.TextField(blank=False, null=False)
+    notification_id = models.TextField(blank=False, null=False)
     created = models.DateTimeField(auto_now_add=True)
