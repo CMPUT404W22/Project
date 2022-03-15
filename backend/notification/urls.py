3b64856a9dcd67c438dd5cd87cc89a3189fbffda
celina <csheng2@ualberta.ca>
2022-03-14 19:20:15 -0600

    move inbox url from authors/url.py to notifcation/url.py for better clarity

--- backend/notification/urls.py
+++ backend/notification/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from notification import views as notificationViews
 
 urlpatterns = [
+    path('<str:user_id>/inbox', notificationViews.NotificationsApiView.as_view(), name = "notifications in inbox")
 ]
