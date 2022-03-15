a4c139cc0aff246ce4b9262f8c8e4157faed212c
celina <csheng2@ualberta.ca>
2022-03-14 23:13:40 -0600

    added follow request model & serializer

--- backend/following/serializer.py
+++ backend/following/serializer.py
@@ -1,15 +1,25 @@
 from rest_framework import serializers
 
 from author.serializer import AuthorSerializer
-from following.models import Following
+from author.models import Author
 
+class FollowRequestModel:
+    def __init__(self, follower: Author, author: Author) -> None:
+        self.follower = follower
+        self.author = author
+    
+    def getSummary(self):
+        return self.follower.display_name + " wants to follow " + self.author.display_name
 
-class FollowingSerializer(serializers.ModelSerializer):
+class FollowRequestSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Following
+        model = FollowRequestModel
         fields = ()
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-
+        representation["type"] = "Follow"
+        representation["summary"] = instance.getSummary()
+        representation["actor"] = AuthorSerializer(instance.follower, many=False).data
+        representation["object"] = AuthorSerializer(instance.author, many=False).data
         return representation
