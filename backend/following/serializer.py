6e8e1246623e6d2407b1c015dd8877f1c70ed22b
celina <csheng2@ualberta.ca>
2022-03-15 01:10:43 -0600

    Add "FollowRequest" table in DB

--- backend/following/serializer.py
+++ backend/following/serializer.py
@@ -2,24 +2,17 @@ from rest_framework import serializers
 
 from author.serializer import AuthorSerializer
 from author.models import Author
-
-class FollowRequestModel:
-    def __init__(self, follower: Author, author: Author) -> None:
-        self.follower = follower
-        self.author = author
-    
-    def getSummary(self):
-        return self.follower.display_name + " wants to follow " + self.author.display_name
+from following.models import FollowRequest
 
 class FollowRequestSerializer(serializers.ModelSerializer):
     class Meta:
-        model = FollowRequestModel
+        model = FollowRequest
         fields = ()
 
-    def to_representation(self, instance):
+    def to_representation(self, instance: FollowRequest):
         representation = super().to_representation(instance)
         representation["type"] = "Follow"
-        representation["summary"] = instance.getSummary()
-        representation["actor"] = AuthorSerializer(instance.follower, many=False).data
-        representation["object"] = AuthorSerializer(instance.author, many=False).data
+        representation["summary"] = instance.get_summary()
+        representation["actor"] = AuthorSerializer(instance.get_requesting_author(), many=False).data
+        representation["object"] = AuthorSerializer(instance.get_author(), many=False).data
         return representation
