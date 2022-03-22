8fd4df405fe0bd1ff97c386a59b508391ee9a5d2
yuqian5 <28016308+yuqian5@users.noreply.github.com>
2022-03-21 19:02:18 -0600

    server side apis

--- backend/like/views.py
+++ backend/like/views.py
@@ -48,8 +48,8 @@ class GetLikeCommentApiView(GenericAPIView):
 
     def get(self, request, user_id, post_id, comment_id):
         try:
-            post = Post.objects.get(id=post_id, author=user_id) # ensure that parameters passed are author's id, post id
-            comment = Comment.objects.get(id=comment_id, post_id=post.id)
+            # post = Post.objects.get(id=post_id, author=user_id) # ensure that parameters passed are author's id, post id
+            comment = Comment.objects.get(id=comment_id)
             likes = LikeComment.objects.filter(comment=comment)
             return response.Response(self.serializer_class(likes, many=True).data, status=status.HTTP_200_OK)
 
